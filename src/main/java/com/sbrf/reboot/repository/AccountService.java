d551be98af520b6498dc442cbc80b4e7d3208748
ivan-ig <ivan_10_93@bk.ru>
2022-01-08 18:26:16 +0300

    Рефакторинг класса AccountService.

--- src/main/java/com/sbrf/reboot/repository/AccountService.java
+++ src/main/java/com/sbrf/reboot/repository/AccountService.java
@@ -1,21 +1,20 @@
 package com.sbrf.reboot.repository;
 
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
+@AllArgsConstructor
 public class AccountService {
 
+    @NonNull
     private final AccountRepository accountRepository;
-    private static final long INVALID_CLIENT_ID = 0L;
-
-    public AccountService(@NonNull AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
 
     public boolean isClientHasContract(long clientId, long contractNumber) {
         return accountRepository.getAllAccountsByClientId(clientId).contains(contractNumber);
     }
 
     public boolean isContractNumberValid(long contractNumber) {
-        return accountRepository.getClientIdByContractNumber(contractNumber) != INVALID_CLIENT_ID;
+        long invalidClientId = 0L;
+        return accountRepository.getClientIdByContractNumber(contractNumber) != invalidClientId;
     }
 }
