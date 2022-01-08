f4c2220b51848251679a87d40f080bf48afed8d0
ivan-ig <ivan_10_93@bk.ru>
2022-01-09 00:47:48 +0300

    Добавлен класс FileAccountRepository, реализующий интерфейс AccountRepository.

--- src/main/java/com/sbrf/reboot/repository/AccountService.java
+++ src/main/java/com/sbrf/reboot/repository/AccountService.java
@@ -3,13 +3,15 @@ package com.sbrf.reboot.repository;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
+import java.io.IOException;
+
 @AllArgsConstructor
 public class AccountService {
 
     @NonNull
     private final AccountRepository accountRepository;
 
-    public boolean isClientHasContract(long clientId, long contractNumber) {
+    public boolean isClientHasContract(long clientId, long contractNumber) throws IOException {
         return accountRepository.getAllAccountsByClientId(clientId).contains(contractNumber);
     }
 
---
+++ src/main/java/com/sbrf/reboot/repository/impl/FileAccountRepository.java
@@ -0,0 +1,64 @@
+package com.sbrf.reboot.repository.impl;
+
+import com.sbrf.reboot.repository.AccountRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.String.valueOf;
+
+@RequiredArgsConstructor
+public class FileAccountRepository implements AccountRepository {
+
+    @NonNull
+    private final String filePath;
+    private final List<String> fileLines = new ArrayList<>();
+
+    @Override
+    public Set<Long> getAllAccountsByClientId(long clientId) throws IOException {
+        readLinesFromFileToList();
+        return parseListLinesAndGetAccountNumbersByClientId(clientId);
+    }
+
+    private void readLinesFromFileToList() throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            while (bufferedReader.ready()) {
+                fileLines.add(bufferedReader.readLine());
+            }
+        }
+    }
+
+    private Set<Long> parseListLinesAndGetAccountNumbersByClientId(long clientId) {
+        String clientIdValue = "";
+        Set<Long> accountNumbers = new HashSet<>();
+
+        for (String line : fileLines) {
+            if (line.contains("clientId")) {
+                String[] clientIdLine = line.split(" ");
+                clientIdValue = clientIdLine[1].replace(',', ' ').trim();
+            }
+            if (clientIdValue.equals(valueOf(clientId)) && line.contains("number")) {
+                String[] lineWithNumber = line.split(" ");
+                try {
+                    Long number = Long.parseLong(lineWithNumber[1]);
+                    accountNumbers.add(number);
+                } catch (NumberFormatException nfe) {
+                    nfe.printStackTrace();
+                }
+            }
+        }
+        return accountNumbers;
+    }
+
+    @Override
+    public long getClientIdByContractNumber(long contractNumber) {
+        throw new RuntimeException("Method is not implemented");
+    }
+}
