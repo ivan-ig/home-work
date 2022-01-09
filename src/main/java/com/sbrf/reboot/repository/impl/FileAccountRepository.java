f912258456b2ab2707550b6d825bcb6adf16332b
ivan-ig <ivan_10_93@bk.ru>
2022-01-09 23:26:15 +0300

    Добавлен метод обновления номера счета и тесты для него

--- src/main/java/com/sbrf/reboot/repository/impl/FileAccountRepository.java
+++ src/main/java/com/sbrf/reboot/repository/impl/FileAccountRepository.java
@@ -4,13 +4,8 @@ import com.sbrf.reboot.repository.AccountRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 import static java.lang.String.valueOf;
 
@@ -19,37 +14,38 @@ public class FileAccountRepository implements AccountRepository {
 
     @NonNull
     private final String filePath;
-    private final List<String> fileLines = new ArrayList<>();
 
     @Override
     public Set<Long> getAllAccountsByClientId(long clientId) {
-        readLinesFromFileToList();
-        return parseListLinesAndGetAccountNumbersByClientId(clientId);
+        List<String> fileLines = readLinesFromFileToList();
+        return parseLinesAndGetAccountNumbersByClientId(fileLines, clientId);
     }
 
-    private void readLinesFromFileToList() {
+    private List<String> readLinesFromFileToList() {
+        List<String> listOfLines = new ArrayList<>(23);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
             while (bufferedReader.ready()) {
-                fileLines.add(bufferedReader.readLine());
+                listOfLines.add(bufferedReader.readLine());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return listOfLines;
     }
 
-    private Set<Long> parseListLinesAndGetAccountNumbersByClientId(long clientId) {
+    private Set<Long> parseLinesAndGetAccountNumbersByClientId(List<String> fileLines, long clientId) {
         String clientIdValue = "";
         Set<Long> accountNumbers = new HashSet<>();
 
         for (String line : fileLines) {
             if (line.contains("clientId")) {
                 String[] clientIdLine = line.split(" ");
-                clientIdValue = clientIdLine[1].replace(',', ' ').trim();
+                clientIdValue = clientIdLine[clientIdLine.length - 1].replace(',', ' ').trim();
             }
             if (clientIdValue.equals(valueOf(clientId)) && line.contains("number")) {
                 String[] lineWithNumber = line.split(" ");
                 try {
-                    Long number = Long.parseLong(lineWithNumber[1]);
+                    Long number = Long.parseLong(lineWithNumber[lineWithNumber.length - 1]);
                     accountNumbers.add(number);
                 } catch (NumberFormatException nfe) {
                     nfe.printStackTrace();
@@ -57,6 +53,38 @@ public class FileAccountRepository implements AccountRepository {
             }
         }
         return accountNumbers;
+    }
+
+    public void updateAccountNumber(long clientId, long oldNumber, long newNumber) throws IOException {
+        List<String> linesForWriting = prepareListLinesForWriting(clientId, oldNumber, newNumber);
+        if (!linesForWriting.isEmpty()) {
+            rewriteFile(linesForWriting);
+        }
+    }
+
+    private List<String> prepareListLinesForWriting(long clientId, long oldNumber, long newNumber) {
+        List<String> fileLines = readLinesFromFileToList();
+        String clientIdLinePattern = "\"clientId\": " + clientId + ",";
+        String oldNumberPattern = "    \"number\": " + oldNumber;
+        String newNumberPattern = "    \"number\": " + newNumber;
+
+        for (int i = 0; i < fileLines.size(); i++){
+            if (fileLines.get(i).contains(clientIdLinePattern)) {
+                i++;
+            }
+            if (fileLines.get(i).equals(oldNumberPattern)) {
+                fileLines.set(i, newNumberPattern);
+            }
+        }
+        return fileLines;
+    }
+
+    private void rewriteFile(List<String> linesForWriting) throws IOException {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
+            for (String line : linesForWriting) {
+                bufferedWriter.write(line+"\n");
+            }
+        }
     }
 
     @Override
--- src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
@@ -3,6 +3,7 @@ package com.sbrf.reboot.repository;
 import com.sbrf.reboot.repository.impl.FileAccountRepository;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -39,4 +40,35 @@ class FileAccountRepositoryTest {
         assertTrue(actualAccounts.isEmpty());
     }
 
+    @Test
+    void updatingAbsentAccountNumber() throws IOException {
+        long clientId = 1L, oldNumber = 3L, newNumber = 555L;
+        String filePath = "src/main/resources/Accounts.txt";
+        FileAccountRepository far = new FileAccountRepository(filePath);
+        far.updateAccountNumber(clientId, oldNumber, newNumber);
+
+        Set<Long> actualAccounts = far.getAllAccountsByClientId(1L);
+        Set<Long> expected = new HashSet<Long>() {{
+            add(111L);
+            add(333L);
+            add(222L);
+        }};
+
+        actualAccounts.forEach(e -> assertTrue(expected.contains(e)));
+    }
+
+    @Test
+    void successfulUpdatingAccountNumber() throws IOException {
+        long clientId = 2L, oldNumber = 777L, newNumber = 123123L;
+        String filePath = "src/main/resources/Accounts.txt";
+        FileAccountRepository far = new FileAccountRepository(filePath);
+        far.updateAccountNumber(clientId, oldNumber, newNumber);
+
+        Set<Long> actualAccounts = far.getAllAccountsByClientId(2L);
+        Set<Long> expected = new HashSet<Long>() {{
+            add(123123L);
+        }};
+
+        actualAccounts.forEach(e -> assertTrue(expected.contains(e)));
+    }
 }
