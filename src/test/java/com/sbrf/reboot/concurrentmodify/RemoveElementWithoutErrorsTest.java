b9582b0454ee6de2b88b1d3a271a98e74942cde9
ivan-ig <ivan_10_93@bk.ru>
2022-01-26 15:14:16 +0300

    Реализовано удаление элемента итерируемой коллекции

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,23 +1,37 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
 
-    @Test
-    public void successConcurrentModificationException() {
+    private List<Integer> list;
 
-        List<Integer> list = new ArrayList() {{
+    @BeforeEach
+    public void setUp() {
+        list = new ArrayList<Integer>() {{
             add(1);
             add(2);
             add(3);
         }};
+    }
+
+    @AfterEach
+    public void reset() {
+        list.clear();
+    }
+
+    @Test
+    public void successConcurrentModificationException() {
 
         assertThrows(ConcurrentModificationException.class, () -> {
             
@@ -32,6 +46,37 @@ public class RemoveElementWithoutErrorsTest {
     @Test
     public void successRemoveElementWithoutErrors() {
 
+        list.removeIf(e -> list.indexOf(e) == 1);
+
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsUsingLegacyMethod() {
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (list.indexOf(element) == 1) {
+                iterator.remove();
+                break;
+            }
+        }
+
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void  successRemoveElementWithoutErrorsUsingCopy() {
+        ArrayList<Integer> listCopy = new ArrayList<>(list);
+
+        for (Integer i : listCopy) {
+            if (Integer.valueOf(2).equals(i)) {
+                list.remove(i);
+            }
+        }
+
+        assertEquals(2, list.size());
     }
 
 }
